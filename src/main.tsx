64a67ba483f10491e5101adbe8b43bb16fc358a4
Antonio-Saucedo <antoniojesus@byui.edu>
2026-07-29 11:54:58 -0600

    Bugs | Changes to fix modal page projects

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 
+window.addEventListener('vite:preloadError', (event) => {
+    console.warn('Vite preload error (non-fatal):', event);
+    event.preventDefault();
+});
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App/>
+    </StrictMode>,
 )
